fa4f77a27f5b12225994dbadd8559c5225eedf77
Noah <noah.terminello@gmail.com>
2024-08-11 11:36:34 -0700

    Added solution for #53

--- 53/main.go
+++ 53/main.go
@@ -1,49 +1,24 @@
-package main
-
-import "fmt"
+package MaximumSumOfSubarray
 
 func maxSubArray(nums []int) int {
+	var globalMaxSum int = nums[0]
 
-	
-	//brainstorming
-	// where do we start?
-	// where do we end?
-	// taking inspiration from #238, what if we had a "running sum" that goes forwards and backwards
-	// once we find the start, we can just loop through the rest of the array sum to see if it gets bigger at all
-	// this does not work
+	localMaxSum := 0
+	for i := range nums {
+		// these statements give the maximum subarray at the ith index
+		if localMaxSum > 0 {
+			localMaxSum += nums[i]
+		} else {
+			localMaxSum = nums[i]
+		}
 
-	// solution space is O(n^2) so a brute force approach is O(n^2)
-	// we need at least an O(n) algo
-
-	// we can try a divide and conquer approach
-	// maximum subarray of array of length 1 is itself
-	// when merging, the subarray is either both combined, or one or the other, depending on the sum
-	// if the sum is negative, we choose the least negative one
-	// if the sum is positive, it's both
-	// I dont think this strategy works, because it assumes they can always be added
-
-	//maybe do above strat, but with an extra bool saying if it can be merged or if it has to be taken by itself
-	// algo
-	// recurse into base case
-	// get sum
-	// merge - if greater is on
-
-	//okay lets just think of an iterative algo first
-
-	//
-
-	result := make([]int, len(nums))
-	
-	var sum int = 0
-	for _, num := range result {
-		fmt.Println(num)
-		sum += num
+		// these statements record the largest sum
+		if globalMaxSum < localMaxSum {
+			globalMaxSum = localMaxSum
+		}
 	}
 
-	fmt.Println("input", nums)
-	fmt.Println("resulting subarray", result)
-	fmt.Println("sum", sum)
-	return sum
+	return globalMaxSum
 }
 
 func main() {
